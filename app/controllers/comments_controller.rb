1ede73053d9eaffc257d9424c74cfc08c81ef132
mlf <micafinn@gmail.com>
2017-04-07 18:29:19 +0200

    Add ability to delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,4 +13,11 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
 end
